380a5ae1fb67156cc80ee3c87347bfb134c994ca
karlagn96 <karlaivonnegn@gmail.com>
2025-01-29 18:17:12 -0600

    deshabilitar eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@next/next/no-img-element": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  })
 ];
 
 export default eslintConfig;
